e9ec956dc51e6ed20c0205bee460df1ce7ff5f36
Kurt Roekle <k_roekle@apple.com>
2026-06-05 17:20:29 -0500

    adding cli for testing
    Signed-off-by: Kurt Roekle <k_roekle@apple.com>

---
+++ cli/src/main/java/io/github/open_policy_agent/opa/cli/Regoj.java
@@ -0,0 +1,18 @@
+/*
+ * Entry point for the regoj CLI.
+ */
+package io.github.open_policy_agent.opa.cli;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(
+    name = "regoj",
+    subcommands = {Eval.class})
+public class Regoj {
+  public static void main(String[] args) {
+    int exitCode =
+        new CommandLine(new Regoj()).setCaseInsensitiveEnumValuesAllowed(true).execute(args);
+    System.exit(exitCode);
+  }
+}
